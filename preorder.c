cc7bbca70c6aa6929d8ad8d2b9563403c2e3c5a2
Samridhi Singh <88153042+samridhi-23@users.noreply.github.com>
2023-05-01 18:02:20 +0530

    Create preorder.c

---
+++ preorder.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include "rtree.h"
+void pre_order_traversal(RTreeNode* node) {
+    if (node != NULL) {
+        // Print the bounds of this node
+        printf("[");
+        printf("%d %d %d %d", node->data.internal.rect.xh, node->data.internal.rect.yh, node->data.internal.rect.xl, node->data.internal.rect.yl);
+        printf("]\n");
+        // Recursively traverse the children of this node
+        if (node->isLeaf == true) {
+            for (int i = 0; i < node->data.leaf.max_hv + 1; i++) {
+                printf("    (%d,%d)\n", node->data.leaf.rect.xh, node->data.leaf.rect.yh);
+            }
+        }
+        else {
+            for (int i = 0; i < node->data.internal.max_hv + 1; i++) {
+                pre_order_traversal(node->data.internal.child[i]);
+            }
+        }
+    }
+}
