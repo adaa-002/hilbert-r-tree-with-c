f035f719757f756477edb21ba950268e3281fca2
Samridhi Singh <88153042+samridhi-23@users.noreply.github.com>
2023-05-02 15:48:09 +0530

    Update search.c

--- search.c
+++ search.c
@@ -4,25 +4,25 @@
 #include <stdbool.h>
 #include <math.h>
 #include "rtree.h"
-bool is_intersect(Rect a, Rect b) {
+bool intersecting(Rect a, Rect b) {
     if (a.xh < b.xl || b.xh < a.xl || a.yh < b.yl || b.yh < a.yl) {
         return false;
     }
     return true;
 }
 
-void search_rtree(RTreeNode* node, Rect search_rect) {
+void search(RTreeNode* node, Rect search_rect) {
     if (node->isLeaf == true) {
         for (int i = 0; i < node->data.leaf.max_hv + 1; i++) {
-            if (is_intersect(node->data.leaf.rect, search_rect)) {
+            if (intersecting(node->data.leaf.rect, search_rect)) {
                 printf("(%d,%d)\n", node->data.leaf.rect.xh, node->data.leaf.rect.yh);
             }
         }
     }
     else {
         for (int i = 0; i < node->data.internal.max_hv + 1; i++) {
-            if (is_intersect(node->data.internal.child[i]->data.internal.rect, search_rect)) {
-                search_rtree(node->data.internal.child[i], search_rect);
+            if (intersecting(node->data.internal.child[i]->data.internal.rect, search_rect)) {
+                search(node->data.internal.child[i], search_rect);
             }
         }
     }
