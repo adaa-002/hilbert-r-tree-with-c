f7755dba10162474c2d7d12279c9be84e396e4ca
Samridhi Singh <88153042+samridhi-23@users.noreply.github.com>
2023-05-02 16:09:17 +0530

    Update search.c

--- search.c
+++ search.c
@@ -13,14 +13,14 @@ bool intersecting(Rect a, Rect b) {
 
 void search(RTreeNode* node, Rect search_rect) {
     if (node->isLeaf == true) {
-        for (int i = 0; i < node->data.leaf.max_hv + 1; i++) {
+        for (int i = 0; i <= node->data.leaf.max_hv ; i++) {
             if (intersecting(node->data.leaf.rect, search_rect)) {
                 printf("(%d,%d)\n", node->data.leaf.rect.xh, node->data.leaf.rect.yh);
             }
         }
     }
     else {
-        for (int i = 0; i < node->data.internal.max_hv + 1; i++) {
+        for (int i = 0; i <= node->data.internal.max_hv ; i++) {
             if (intersecting(node->data.internal.child[i]->data.internal.rect, search_rect)) {
                 search(node->data.internal.child[i], search_rect);
             }
